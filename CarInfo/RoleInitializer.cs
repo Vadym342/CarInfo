c35585d730f65716d89f1d9f4cb649abce99aac8
BLABLABLA <vadimca1.boyarchuc@gmail.com>
2020-09-30 18:07:36 +0300

    Add roles

---
+++ CarInfo/RoleInitializer.cs
@@ -0,0 +1,37 @@
+﻿using CarInfo.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarInfo
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string adminEmail = "admin@gmail.com";
+            string password = "_Aa123456";
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("employee") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("employee"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+        }
+
+
+    }
+}
