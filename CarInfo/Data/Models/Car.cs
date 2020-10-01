af8c4016bec2148e5125ca45c9cf72d0ebaa2d53
BLABLABLA <vadimca1.boyarchuc@gmail.com>
2020-10-01 15:52:33 +0300

    Add files

---
+++ CarInfo/Data/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarInfo.Data.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+
+        public string mark { get; set; }
+        public string name { get; set; }
+        public string shortDesc { get; set; }
+        public string longDesc { get; set; }
+        public string img { get; set; }
+        public uint price { get; set; }
+        public bool isFavourite { get; set; }
+        public bool avaible { get; set; }
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
